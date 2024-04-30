94977a60c4149ddd48f8bb2e15bad08ce3f707b4
Ezebed <obedvillegas.02@gmail.com>
2024-04-30 14:59:54 -0400

    install breeze authentication with react

--- resources/js/Pages/Layout.jsx
+++ resources/js/Pages/Layout.jsx
@@ -1,13 +1,12 @@
-import { Link } from '@inertiajs/react'
 import React from 'react'
 
 export default function Layout({ children }) {
   return (
     <main>
-      <header>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+      <header className='flex gap-x-2'>
+        <a className="px-4 py-2 bg-indigo-100 hover:bg-indigo-400 transition-all rounded-md" href="/">Home</a>
+        <a className="px-4 py-2 bg-indigo-100 hover:bg-indigo-400 transition-all rounded-md" href="/about">About</a>
+        <a className="px-4 py-2 bg-indigo-100 hover:bg-indigo-400 transition-all rounded-md" href="/contact">Contact</a>
       </header>
       <article>{children}</article>
     </main>
