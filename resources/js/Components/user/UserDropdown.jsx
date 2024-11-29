ab8bdacc7e66aa1f930a680c612a1694c8321698
Ezebed <obedvillegas.02@gmail.com>
2024-11-29 17:56:05 -0400

    extrac routeslist from asideDrawer and optimize hasPermission function with useCallback

--- resources/js/Components/user/UserDropdown.jsx
+++ resources/js/Components/user/UserDropdown.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import LogoutIcon from '@mui/icons-material/Logout';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-import { Link } from '@inertiajs/react';
+import { Link } from "@inertiajs/react";
 
-export default function UserDropdown({user})
-{
+import { useTranslation } from "react-i18next";
+
+export default function UserDropdown({ user }) {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
 
@@ -23,47 +24,45 @@ export default function UserDropdown({user})
         setAnchorEl(null);
     };
 
-    return(
-        <Box sx={{ display:{ xs: 'none', sm: 'block' } }}>
+    const { t } = useTranslation(["translation"]);
+
+    return (
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
             <Button
-                id='userDropdown'
-                aria-controls={open ? 'user-menu': undefined}
+                id="userDropdown"
+                aria-controls={open ? "user-menu" : undefined}
                 aria-haspopup="true"
-                aria-expanded={open ? 'true': undefined}
+                aria-expanded={open ? "true" : undefined}
                 onClick={handleClick}
             >
                 {user.name}
             </Button>
             <Menu
-                id='user-menu'
+                id="user-menu"
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
                 MenuListProps={{
-                    'aria-labelledby': 'user-menu',
+                    "aria-labelledby": "user-menu",
                 }}
             >
-                <Link href={route('profile.edit')}>
+                <Link href={route("profile.edit")}>
                     <MenuItem onClick={handleClose}>
                         <ListItemIcon>
                             <AccountCircleIcon />
                         </ListItemIcon>
-                        <ListItemText>
-                            Perfil
-                        </ListItemText>
+                        <ListItemText>{t("Profile")}</ListItemText>
                     </MenuItem>
                 </Link>
-                <Link href={route('logout')} method="post" as="button" >
+                <Link href={route("logout")} method="post" as="button">
                     <MenuItem onClick={handleClose}>
-                    <ListItemIcon>
+                        <ListItemIcon>
                             <LogoutIcon />
                         </ListItemIcon>
-                        <ListItemText>
-                            Finalizar Sesión
-                        </ListItemText>
+                        <ListItemText>{t("Log Out")}</ListItemText>
                     </MenuItem>
                 </Link>
             </Menu>
         </Box>
-    )
+    );
 }
